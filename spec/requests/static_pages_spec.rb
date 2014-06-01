ccc51edef4a5674631e46f4f6db3bc68a2581dab
Gerard Sweeney <gerard0sweeney@gmail.com>
2014-06-01 22:29:51 +0100

    tweaked some tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,35 +2,46 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject { page }
-
+  
+  shared_name = "all static pages"
+  
+  shared_examples_for shared_name do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+  
   describe "Home page" do
     before { visit root_path }
+    let(:heading) {"Sample App"}
+    let(:page_title) {""}
     
-    it {should have_content('Sample App')}
-    it {should have_title(full_title(""))}
+    it_should_behave_like(shared_name)
     it {should_not have_title("| Home")}
     
   end
   
   describe "Help page" do
     before { visit help_path }
-    it {should have_content('Help')}
-    it {should have_title(full_title("Help"))}
+    let(:heading) {"Help"}
+    let(:page_title) {"Help"}
     
+    it_should_behave_like(shared_name)
   end
   
   describe "About page" do
     before {visit about_path}
-    it {should have_content('About Us')}
-    it {should have_title(full_title("About"))}
+    let(:heading) {"About Us"}
+    let(:page_title) {"About"}
     
+    it_should_behave_like shared_name
   end
   
   describe "Contact page" do
     before {visit contact_path}
-    it {should have_content('Contact')}
-    it {should have_title(full_title("Contact"))}
+    let(:heading) {"Contact"}
+    let(:page_title) {"Contact"}
     
+    it_should_behave_like shared_name
   end
   
 end
